034eecb5484d178a111519882fcbf266409ab7e0
Jonas Sanchez <jsanchez0000.js@gmail.com>
2026-04-22 14:06:50 -0700

    Disable driver monitoring events

--- selfdrive/monitoring/helpers.py
+++ selfdrive/monitoring/helpers.py
@@ -326,6 +326,9 @@ class DriverMonitoring:
 
   def _update_events(self, driver_engaged, op_engaged, standstill, wrong_gear, car_speed):
     self._reset_events()
+    # DM disabled: short-circuit so no driverDistracted/driverUnresponsive/tooDistracted events
+    # are ever raised. Pipeline keeps running so downstream consumers don't break.
+    return
     # Block engaging until ignition cycle after max number or time of distractions
     if self.terminal_alert_cnt >= self.settings._MAX_TERMINAL_ALERTS or \
        self.terminal_time >= self.settings._MAX_TERMINAL_DURATION:
